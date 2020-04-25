35c74636862c083abe1f44a72e9c0f34fe8c4b00
JGR <jordi.id96@gmail.com>
2020-04-26 00:21:55 +0200

    BDN9: Enable lighting layers

--- keyboards/bdn9/config.h
+++ keyboards/bdn9/config.h
@@ -65,3 +65,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define LOCKING_SUPPORT_ENABLE
 /* Locking resynchronize hack */
 #define LOCKING_RESYNC_ENABLE
+
+// Enable lighting layers
+#define RGBLIGHT_LAYERS
