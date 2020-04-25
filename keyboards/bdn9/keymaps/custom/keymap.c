dccdb303f48e71a9f713933fba84a7035f4e5149
JGR <jordi.id96@gmail.com>
2020-04-26 00:08:41 +0200

    BDN9: rotary encoder macros

--- keyboards/bdn9/keymaps/custom/keymap.c
+++ keyboards/bdn9/keymaps/custom/keymap.c
@@ -16,35 +16,44 @@
 #include QMK_KEYBOARD_H
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    /*
-        | Knob 1: Vol Dn/Up |      | Knob 2: Page Dn/Up |
-        | Press: Mute       | Home | Press: Play/Pause  |
-        | Hold: Layer 2     | Up   | RGB Mode           |
-        | Left              | Down | Right              |
-     */
+
     [0] = LAYOUT(
         KC_MUTE, KC_HOME, KC_MPLY,
         MO(1)  , KC_UP  , RGB_MOD,
         KC_LEFT, KC_DOWN, KC_RGHT
     ),
-    /*
-        | RESET          | N/A  | Media Stop |
-        | Held: Layer 2  | Home | RGB Mode   |
-        | Media Previous | End  | Media Next |
-     */
+
     [1] = LAYOUT(
         RESET  , BL_STEP, KC_STOP,
         _______, KC_HOME, RGB_MOD,
         KC_MPRV, KC_END , KC_MNXT
     ),
+
+    [2] = LAYOUT(
+        RESET  , BL_STEP, KC_STOP,
+        _______, KC_HOME, RGB_MOD,
+        KC_MPRV, KC_END , KC_MNXT
+    ),
+
 };
 
 void encoder_update_user(uint8_t index, bool clockwise) {
     if (index == 1) {
-        if (clockwise) {
-            tap_code(KC_MS_WH_RIGHT);
-        } else {
-            tap_code(KC_MS_WH_LEFT);
+
+        if (IS_LAYER_ON(0)) {
+            if (clockwise) tap_code(KC_MS_WH_RIGHT);
+            else tap_code(KC_MS_WH_LEFT);
         }
+
+        if (IS_LAYER_ON(1)) {
+            if (clockwise) tap_code(LALT(LCTL(KC_1)));
+            else tap_code(LALT(LCTL(KC_2)));
+        }
+
+        if (IS_LAYER_ON(2)) {
+            if (clockwise) tap_code(LALT(LCTL(KC_3)));
+            else tap_code(LALT(LCTL(KC_4)));
+        }
+
     }
 }
