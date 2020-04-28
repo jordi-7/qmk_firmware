7f05f300f401e69ea61c6aca768903422686368b
JGR <jordi.id96@gmail.com>
2020-04-29 00:59:02 +0200

    Zeal60: set capslock led indicator

--- keyboards/zeal60/config.h
+++ keyboards/zeal60/config.h
@@ -110,8 +110,8 @@
 #define RGB_BACKLIGHT_ALPHAS_MODS_ROW_3 0b1111000000000011
 #define RGB_BACKLIGHT_ALPHAS_MODS_ROW_4 0b0011110000000111
 
-#define RGB_BACKLIGHT_CAPS_LOCK_INDICATOR { .color = { .h = 0, .s = 0 }, .index = 255 }
-#define RGB_BACKLIGHT_LAYER_1_INDICATOR { .color = { .h = 0, .s = 0 }, .index = 54+16 }
+#define RGB_BACKLIGHT_CAPS_LOCK_INDICATOR { .color = { .h = 0, .s = 0 }, .index = 8 }
+#define RGB_BACKLIGHT_LAYER_1_INDICATOR { .color = { .h = 0, .s = 0 }, .index = 70 }
 #define RGB_BACKLIGHT_LAYER_2_INDICATOR { .color = { .h = 0, .s = 0 }, .index = 255 }
 #define RGB_BACKLIGHT_LAYER_3_INDICATOR { .color = { .h = 0, .s = 0 }, .index = 255 }
 
