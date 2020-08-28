24aec5a0d17db55da4772d5273499c926d9915da
JGR <jordi.id96@gmail.com>
2020-08-28 14:29:47 +0200

    cleanup

--- keyboards/lily58/keymaps/jgr/keymap.c
+++ keyboards/lily58/keymaps/jgr/keymap.c
@@ -10,8 +10,12 @@ static bool led_on = true;
 static uint8_t led_timeout = 10; // Minutes
 static long int oled_timeout = 600000; // 10 minutes
 
+//
+// Keymaps
+//
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
+    // Layer 0
     [0] = LAYOUT(
     KC_GESC,            KC_1,       KC_2,       KC_3,       KC_4,       KC_5,                                       KC_6,       KC_7,       KC_8,       KC_9,       KC_0,       KC_MINS,
     KC_TAB,             KC_Q,       KC_W,       KC_E,       KC_R,       KC_T,                                       KC_Y,       KC_U,       KC_I,       KC_O,       KC_P,       KC_EQL,
@@ -20,6 +24,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                 KC_LCTL,    KC_LGUI,    KC_LAPO,    KC_BSPACE,          KC_SPC,     KC_RAPC,    TT(1),      KC_RCTL
     ),
 
+    // Layer 1
     [1] = LAYOUT(
     KC_F1,              KC_F2,      KC_F3,      KC_F4,      KC_F5,      KC_F6,                                      KC_F7,      KC_F8,      KC_F9,      KC_F10,     KC_F11,     KC_F12,
     KC_GRV,             KC_VOLD,    KC_UP,      KC_VOLU,    KC_MUTE,    KC_TRNS,                                    KC_HOME,    KC_PGUP,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_WWW_REFRESH,
