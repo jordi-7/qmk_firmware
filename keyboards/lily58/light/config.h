7b06260ce968d58e5158e0546bdb42c62a099e67
JGR <jordi.id96@gmail.com>
2021-01-19 14:26:34 +0100

    Update lily58

--- keyboards/lily58/light/config.h
+++ keyboards/lily58/light/config.h
@@ -2,6 +2,7 @@
 Copyright 2012 Jun Wako <wakojun@gmail.com>
 Copyright 2015 Jack Humbert
 Copyright 2017 F_YUUCHI
+Copyright 2020 Ben Roesner (keycapsss.com)
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -20,12 +21,11 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #pragma once
 
 /* USB Device descriptor parameter */
-#define VENDOR_ID       0x04D8
-#define PRODUCT_ID      0xEB2D
+#define VENDOR_ID       0x7983
+#define PRODUCT_ID      0x4C4C // "LL"
 #define DEVICE_VER      0x0100
-#define MANUFACTURER    liliums
-#define PRODUCT         Lily58
-#define DESCRIPTION     Lily58 is 6×4+5keys column-staggered split keyboard.
+#define MANUFACTURER    Keycapsss
+#define PRODUCT         Lily58L
 
 /* key matrix size */
 // Rows are doubled-up
@@ -36,36 +36,18 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define MATRIX_ROW_PINS { C6, D7, E6, B4, B5 }
 #define MATRIX_COL_PINS { F6, F7, B1, B3, B2, B6 }
 
-
-/* define if matrix has ghost */
-//#define MATRIX_HAS_GHOST
-
 /* Set 0 if debouncing isn't needed */
 #define DEBOUNCE 5
 
-/* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
-//#define LOCKING_SUPPORT_ENABLE
-/* Locking resynchronize hack */
-//#define LOCKING_RESYNC_ENABLE
+#define SOFT_SERIAL_PIN D2
 
-/* ws2812 RGB LED */
 #define RGB_DI_PIN D3
-#define RGBLED_NUM 12    // Number of LEDs
+#define RGBLED_SPLIT { 35, 35 }
+#define RGBLED_NUM 70
+#define RGBLIGHT_SPLIT
+#define RGBLIGHT_LIMIT_VAL 120
 
-/*
- * Feature disable options
- *  These options are also useful to firmware size reduction.
- */
-
-/* disable debug print */
-// #define NO_DEBUG
-
-/* disable print */
-// #define NO_PRINT
-
-/* disable action features */
-//#define NO_ACTION_LAYER
-//#define NO_ACTION_TAPPING
-//#define NO_ACTION_ONESHOT
-//#define NO_ACTION_MACRO
-//#define NO_ACTION_FUNCTION
+#define ENCODERS_PAD_A { F4 }
+#define ENCODERS_PAD_B { F5 }
+#define ENCODERS_PAD_A_RIGHT { F5 }
+#define ENCODERS_PAD_B_RIGHT { F4 }
